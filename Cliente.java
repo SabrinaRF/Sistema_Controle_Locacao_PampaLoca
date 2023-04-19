c78c06ec41b4b36f08426be8e18df884cdebd48a
DaniellyCN <89779359+DaniellyCN@users.noreply.github.com>
2023-04-19 19:13:06 -0300

    inicio

---
+++ Cliente.java
@@ -0,0 +1,15 @@
+public class Cliente{
+    private String nome;
+    private String CNH;
+    private int telefone;
+    private int CPF;
+    
+    public Cliente(String nome, String CNH, int telefone, int CPF){
+        this.nome = nome;
+        this.CNH = CNH;
+        this.telefone = telefone;
+        this.CPF = CPF;
+     }
+
+     
+}
